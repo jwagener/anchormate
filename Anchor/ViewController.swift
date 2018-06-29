f4359dad20db08b6924e8a1fb9aab73db17c2806
Johannes Wagener <johannes@wagener.cc>
2018-06-29 23:10:22 +0200

    Initial Commit

---
+++ Anchor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anchor
+//
+//  Created by Johannes Wagener on 29.06.18.
+//  Copyright © 2018 Johannes Wagener. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
