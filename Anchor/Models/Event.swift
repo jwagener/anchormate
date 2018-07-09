a80634747a31b7b5746f91dce8b6976efdf3c13c
Johannes Wagener <johannes@wagener.cc>
2018-07-09 10:37:42 +0200

    remove event entitiy

--- Anchor/Models/Event.swift
+++
@@ -1,9 +0,0 @@
-import CoreData
-
-class Event: NSManagedObject {
-    static var entityName = "Event"
-    class func new(in context: NSManagedObjectContext) -> Event {
-        return NSEntityDescription.insertNewObject(forEntityName: Event.entityName, into: context) as! Event
-    }
-
-}
